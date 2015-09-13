c9149db8018749b50f430af4892bb846e9d0ab10
AndreiDegtiarev <Andrey.Degtyarev@gmail.com>
2015-09-13 22:13:34 +0200

    build process update

--- build.bat
+++ build.bat
@@ -5,4 +5,5 @@ rem "C:\Program Files (x86)\Microsoft Visual Studio 10.0\Common7\IDE\devenv.exe"
 "C:\Program Files (x86)\Microsoft Visual Studio 10.0\Common7\IDE\devenv.exe" Examples\AWind\Oscilloscope\Oscilloscope.vcxproj /RunExit
 "C:\Program Files (x86)\Microsoft Visual Studio 10.0\Common7\IDE\devenv.exe" Examples\AWind\Text\Text.vcxproj /RunExit
 "C:\Program Files (x86)\Microsoft Visual Studio 10.0\Common7\IDE\devenv.exe" Examples\AWind\WindowSelector\WindowSelector.vcxproj /RunExit
+"C:\Program Files (x86)\Microsoft Visual Studio 10.0\Common7\IDE\devenv.exe" Examples\AWind\Dialogs\Dialogs.vcxproj /RunExit
 "C:\Program Files (x86)\Microsoft Visual Studio 10.0\Common7\IDE\devenv.exe" Examples\AWind\SensorsMonitor\SensorsMonitor.vcxproj /RunExit
