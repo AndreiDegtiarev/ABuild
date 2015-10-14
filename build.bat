2bff4100a08eda11d79bde5d305f26a67bd7716f
AndreiDegtiarev <Andrey.Degtyarev@gmail.com>
2015-10-14 22:58:34 +0200

    Mobile keyboard from lumox

--- build.bat
+++ build.bat
@@ -6,4 +6,5 @@ rem "C:\Program Files (x86)\Microsoft Visual Studio 10.0\Common7\IDE\devenv.exe"
 "C:\Program Files (x86)\Microsoft Visual Studio 10.0\Common7\IDE\devenv.exe" Examples\AWind\Text\Text.vcxproj /RunExit
 "C:\Program Files (x86)\Microsoft Visual Studio 10.0\Common7\IDE\devenv.exe" Examples\AWind\WindowSelector\WindowSelector.vcxproj /RunExit
 "C:\Program Files (x86)\Microsoft Visual Studio 10.0\Common7\IDE\devenv.exe" Examples\AWind\Dialogs\Dialogs.vcxproj /RunExit
+"C:\Program Files (x86)\Microsoft Visual Studio 10.0\Common7\IDE\devenv.exe" Examples\AWind\AWind_04_MobileKB\AWind_04_MobileKB.vcxproj /RunExit
 "C:\Program Files (x86)\Microsoft Visual Studio 10.0\Common7\IDE\devenv.exe" Examples\AWind\SensorsMonitor\SensorsMonitor.vcxproj /RunExit
